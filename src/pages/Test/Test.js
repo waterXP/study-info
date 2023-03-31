3f4bec5bb2fc6a88eb66eb4d4320c022b36d95b2
陈文杰 <wutianba@sina.com>
2023-03-31 18:30:22 +0800

    up: updfiles

---
+++ src/pages/Test/Test.js
@@ -0,0 +1,10 @@
+import React, { memo } from 'react'
+// import PropTypes from 'prop-types'
+import './Test.styl'
+
+const Test = () =>
+  <div className='test'>test</div>
+Test.propTypes = {
+}
+
+export default memo(Test)

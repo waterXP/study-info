4c8ec6312876726c704b5ab9c0904a08dd6957cd
陈文杰 <wutianba@sina.com>
2023-05-05 20:09:50 +0800

    up: update itto

--- src/consts/itto.js
+++ src/consts/itto.js
@@ -1,9 +1,23 @@
 import ittoIntegration from '@/consts/itto-integration'
 import ittoRange from '@/consts/itto-range'
 import ittoProcess from '@/consts/itto-process'
+import ittoCost from '@/consts/itto-cost'
+import ittoQuality from '@/consts/itto-quality'
+import ittoResource from '@/consts/itto-resource'
+import ittoCommunicate from '@/consts/itto-communicate'
+import ittoRisk from '@/consts/itto-risk'
+import ittoPurchase from '@/consts/itto-purchase'
+import ittoPeople from '@/consts/itto-people'
 
 export default {
   ...ittoIntegration,
   ...ittoRange,
-  ...ittoProcess
+  ...ittoProcess,
+  ...ittoCost,
+  ...ittoQuality,
+  ...ittoResource,
+  ...ittoCommunicate,
+  ...ittoRisk,
+  ...ittoPurchase,
+  ...ittoPeople
 }

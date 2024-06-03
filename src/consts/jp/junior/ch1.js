deb454e4609f8682842d3d460f72947d5648010e
waterXP <wutianba@sina.com>
2024-06-03 15:45:31 +0800

    up: udpate files

---
+++ src/consts/jp/junior/ch1.js
@@ -0,0 +1,461 @@
+// [
+//   {
+//     chapter: 1,
+//     title: '',
+//     lesson: [
+//       {
+//         no: 1,
+//         topic: '',
+//         basic: [],
+//         utilization: [],
+//         grammar: [],
+//         word: [
+//           {
+//             kana: '',
+//             mana: '',
+//             type: '',
+//             cn: ''
+//           }
+//         ]
+//       },
+//       {
+//         no: 2,
+//         topic: '',
+//         basic: [],
+//         utilization: [],
+//         grammar: [],
+//         word: [
+//           {
+//             kana: '',
+//             mana: '',
+//             type: '',
+//             cn: ''
+//           }
+//         ]
+//       },
+//       {
+//         no: 3,
+//         topic: '',
+//         basic: [],
+//         utilization: [],
+//         grammar: [],
+//         word: [
+//           {
+//             kana: '',
+//             mana: '',
+//             type: '',
+//             cn: ''
+//           }
+//         ]
+//       },
+//       {
+//         no: 4,
+//         topic: '',
+//         basic: [],
+//         utilization: [],
+//         grammar: [],
+//         word: [
+//           {
+//             kana: '',
+//             mana: '',
+//             type: '',
+//             cn: ''
+//           }
+//         ]
+//       }
+//     ]
+//   }
+// ]
+
+export default [
+  {
+    chapter: 1,
+    title: '',
+    lesson: [
+      {
+        no: 1,
+        topic: '李さんは　中国人です',
+        basic: [],
+        utilization: [],
+        grammar: [],
+        word: [
+          {
+            kana: 'ちゅうごくじん',
+            mana: '中国人',
+            type: '名',
+            cn: '中国人'
+          },
+          {
+            kana: 'にほんじん',
+            mana: '日本人',
+            type: '名',
+            cn: '日本人'
+          },
+          {
+            kana: 'かんこくじん',
+            mana: '韓国人',
+            type: '名',
+            cn: '韩国人'
+          },
+          {
+            kana: 'アメリカじん',
+            mana: 'アメリカ人',
+            type: '名',
+            cn: '美国人'
+          },
+          {
+            kana: 'フランスじん',
+            mana: 'フランス人',
+            type: '名',
+            cn: '法国人'
+          },
+          {
+            kana: 'がくせい',
+            mana: '学生',
+            type: '名',
+            cn: '（大）学生'
+          },
+          {
+            kana: 'せんせい',
+            mana: '先生',
+            type: '名',
+            cn: '老师'
+          },
+          {
+            kana: 'りゅうがくせい',
+            mana: '留学生',
+            type: '名',
+            cn: '留学生'
+          },
+          {
+            kana: 'きょうじゅ',
+            mana: '教授',
+            type: '名',
+            cn: '教授'
+          },
+          {
+            kana: 'しゃいん',
+            mana: '社員',
+            type: '名',
+            cn: '职员'
+          },
+          {
+            kana: 'かいしゃいん',
+            mana: '会社員',
+            type: '名',
+            cn: '公司职员'
+          },
+          {
+            kana: 'てんいん',
+            mana: '店員',
+            type: '名',
+            cn: '店员'
+          },
+          {
+            kana: 'けんしゅうせい',
+            mana: '研修生',
+            type: '名',
+            cn: '进修生'
+          },
+          {
+            kana: 'きぎょう',
+            mana: '企業',
+            type: '名',
+            cn: '企业'
+          },
+          {
+            kana: 'だいがく',
+            mana: '大学',
+            type: '名',
+            cn: '大学'
+          },
+          {
+            kana: 'ちち',
+            mana: '父',
+            type: '名',
+            cn: '（我）父亲'
+          },
+          {
+            kana: 'かちょう',
+            mana: '課長',
+            type: '名',
+            cn: '科长'
+          },
+          {
+            kana: 'しゃちょう',
+            mana: '社長',
+            type: '名',
+            cn: '总经理，社长'
+          },
+          {
+            kana: 'でむかえ',
+            mana: '出迎え',
+            type: '名',
+            cn: '迎接'
+          },
+          {
+            kana: 'あのひと',
+            mana: 'あの人',
+            type: '名',
+            cn: '那个人'
+          },
+          {
+            kana: 'わたし',
+            mana: '',
+            type: '代',
+            cn: '我'
+          },
+          {
+            kana: 'あなた',
+            mana: '',
+            type: '代',
+            cn: '你'
+          },
+          {
+            kana: 'どうも',
+            mana: '',
+            type: '副',
+            cn: '非常，很'
+          },
+          {
+            kana: 'はい',
+            mana: '',
+            type: '叹',
+            cn: '哎，是（应答）；是的'
+          },
+          {
+            kana: 'いいえ',
+            mana: '',
+            type: '叹',
+            cn: '不，不是'
+          },
+          {
+            kana: 'あっ',
+            mana: '',
+            type: '叹',
+            cn: '哎，哎呀'
+          },
+          {
+            kana: 'り',
+            mana: '李',
+            type: '专',
+            cn: '李'
+          },
+          {
+            kana: 'おう',
+            mana: '王',
+            type: '专',
+            cn: '王'
+          },
+          {
+            kana: 'ちょう',
+            mana: '張',
+            type: '专',
+            cn: '张'
+          },
+          {
+            kana: 'もり',
+            mana: '森',
+            type: '专',
+            cn: '森'
+          },
+          {
+            kana: 'はやし',
+            mana: '林',
+            type: '专',
+            cn: '林'
+          },
+          {
+            kana: 'おの',
+            mana: '小野',
+            type: '专',
+            cn: '小野'
+          },
+          {
+            kana: 'よしだ',
+            mana: '吉田',
+            type: '专',
+            cn: '吉田'
+          },
+          {
+            kana: 'たなか',
+            mana: '田中',
+            type: '专',
+            cn: '田中'
+          },
+          {
+            kana: 'なかむら',
+            mana: '中村',
+            type: '专',
+            cn: '中村'
+          },
+          {
+            kana: 'たろう',
+            mana: '太郎',
+            type: '专',
+            cn: '太郎'
+          },
+          {
+            kana: 'キム',
+            mana: '金',
+            type: '专',
+            cn: '金'
+          },
+          {
+            kana: 'ヂュポン',
+            mana: '',
+            type: '专',
+            cn: '迪蓬'
+          },
+          {
+            kana: 'スミス',
+            mana: '',
+            type: '专',
+            cn: '史密斯'
+          },
+          {
+            kana: 'ジョンソン',
+            mana: '',
+            type: '专',
+            cn: '约翰逊'
+          },
+          {
+            kana: 'ちゅうごく',
+            mana: '中国',
+            type: '专',
+            cn: '中国'
+          },
+          {
+            kana: 'とうきょうだいがく',
+            mana: '東京大学',
+            type: '专',
+            cn: '东京大学'
+          },
+          {
+            kana: 'ペキンだいがく',
+            mana: '北京大学',
+            type: '专',
+            cn: '北京大学'
+          },
+          {
+            kana: 'ジェーシーきがく',
+            mana: 'JC企画',
+            type: '专',
+            cn: 'JC策划公司'
+          },
+          {
+            kana: 'ペキンりょこうしゃ',
+            mana: '北京旅行社',
+            type: '专',
+            cn: '北京旅行社'
+          },
+          {
+            kana: 'にっちゅうしょうじ',
+            mana: '日中商事',
+            type: '专',
+            cn: '日中商社'
+          }
+        ],
+        phrase: [
+          {
+            kana: 'こんにちは',
+            mana: '',
+            cn: '你好'
+          },
+          {
+            kana: 'すみません',
+            mana: '',
+            cn: '对不起，请问'
+          },
+          {
+            kana: 'どうぞ',
+            mana: '',
+            cn: '请'
+          },
+          {
+            kana: 'よろしくおねがいします',
+            mana: 'よろしくお願いします',
+            cn: '请多关照'
+          },
+          {
+            kana: 'はじめまして',
+            mana: '',
+            cn: '初次见面'
+          },
+          {
+            kana: 'こちらこそ',
+            mana: '',
+            cn: '我才要（请您～）'
+          },
+          {
+            kana: 'そうです',
+            mana: '',
+            cn: '是（这样）'
+          },
+          {
+            kana: 'ちがいます',
+            mana: '',
+            cn: '不是'
+          },
+          {
+            kana: 'わかりません',
+            mana: '分かりません',
+            cn: '不知道'
+          },
+          {
+            kana: 'どうもすみません',
+            mana: '',
+            cn: '实在对不起'
+          },
+          {
+            kana: '〜さん｜〜ちゃん｜〜君',
+            mana: '',
+            cn: ''
+          }
+        ]
+      },
+      {
+        no: 2,
+        topic: 'これは　本です',
+        basic: [],
+        utilization: [],
+        grammar: [],
+        word: [
+          {
+            kana: '',
+            mana: '',
+            cn: ''
+          }
+        ]
+      },
+      {
+        no: 3,
+        topic: 'ここは　デパートです',
+        basic: [],
+        utilization: [],
+        grammar: [],
+        word: [
+          {
+            kana: '',
+            mana: '',
+            cn: ''
+          }
+        ]
+      },
+      {
+        no: 4,
+        topic: '部屋に　机と　いすが　あります',
+        basic: [],
+        utilization: [],
+        grammar: [],
+        word: [
+          {
+            kana: '',
+            mana: '',
+            cn: ''
+          }
+        ]
+      }
+    ]
+  }
+]

4719c14d88d79c4120073de580c91f117048a30f
waterXP <wutianba@sina.com>
2024-06-18 17:37:36 +0800

    up: udpate intermediate 1~3

--- src/consts/jp/intermediate/ch02.js
+++ src/consts/jp/intermediate/ch02.js
@@ -12,79 +12,159 @@ export default {
       words: [
         {
           word: [
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' }
+            { kana: 'コンテスト', mana: '', type: '', cn: '' },
+            { kana: 'さいこうしょう', mana: '最高賞', type: '', cn: '' },
+            { kana: 'じゅしょうする', mana: '受賞する', type: '', cn: '' },
+            { kana: 'きっかけ', mana: '', type: '', cn: '' },
+            { kana: 'きんせい', mana: '金星', type: '', cn: '' },
+            { kana: 'きんぼし', mana: '金星', type: '', cn: '' },
+            { kana: 'ほし', mana: '星', type: '', cn: '' },
+            { kana: 'すもう', mana: '相撲', type: '', cn: '' },
+            { kana: 'くらい', mana: '位', type: '', cn: '' },
+            { kana: 'りきし', mana: '力士', type: '', cn: '' },
+            { kana: 'よこづな', mana: '横綱', type: '', cn: '' },
+            { kana: 'ボトル', mana: '', type: '', cn: '' },
+            { kana: 'まちがいない', mana: '間違いない', type: '', cn: '' }
           ],
           phrase: [
-            { kana: '', mana: '', cn : '' },
-            { kana: '', mana: '', cn : '' },
-            { kana: '', mana: '', cn : '' }
+            { kana: 'きんぼしをあげる', mana: '金星をあげる', cn : '' },
+            { kana: 'さい〜', mana: '最〜', cn : '' },
+            { kana: '〜しょう', mana: '〜賞', cn : '' }
           ]
         }, {
           word: [
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' }
+            { kana: 'さて', mana: '', type: '', cn: '' },
+            { kana: 'けれども', mana: '', type: '', cn: '' },
+            { kana: 'あと', mana: '', type: '', cn: '' },
+            { kana: 'で', mana: '', type: '', cn: '' },
+            { kana: 'けっか', mana: '結果', type: '', cn: '' },
+            { kana: 'さいだい', mana: '最大', type: '', cn: '' },
+            { kana: 'さいしょう', mana: '最小', type: '', cn: '' },
+            { kana: 'さいてい', mana: '最低', type: '', cn: '' },
+            { kana: 'さいてき', mana: '最適', type: '', cn: '' },
+            { kana: 'さいあい', mana: '最愛', type: '', cn: '' },
+            { kana: 'てがかり', mana: '手がかり', type: '', cn: '' },
+            { kana: 'たより', mana: '頼り', type: '', cn: '' },
+            { kana: 'のこす', mana: '残す', type: '', cn: '' },
+            { kana: 'ナイフ', mana: '', type: '', cn: '' },
+            { kana: 'かいけつする', mana: '解決する', type: '', cn: '' },
+            { kana: 'じゅうどう', mana: '柔道', type: '', cn: '' },
+            { kana: 'ゆらいする', mana: '由来する', type: '', cn: '' },
+            { kana: 'ポルトガルご', mana: 'ポルトガル語', type: '', cn: '' },
+            { kana: 'びん', mana: '瓶', type: '', cn: '' },
+            { kana: 'ワンピース', mana: '', type: '', cn: '' },
+            { kana: 'たいそう', mana: '体操', type: '', cn: '' },
+            { kana: 'ひとりひとり', mana: '1人1人', type: '', cn: '' },
+            { kana: 'ぜいきん', mana: '税金', type: '', cn: '' },
+            { kana: 'おさめる', mana: '納める', type: '', cn: '' },
+            { kana: 'ぎむ', mana: '義務', type: '', cn: '' },
+            { kana: 'そうい', mana: '相違', type: '', cn: '' },
+            { kana: 'ガイドプック', mana: '', type: '', cn: '' },
+            { kana: 'ほうふ', mana: '豊富', type: '', cn: '' }
           ],
           phrase: [
-            { kana: '', mana: '', cn : '' },
-            { kana: '', mana: '', cn : '' },
-            { kana: '', mana: '', cn : '' }
+            { kana: 'ちがいない', mana: '違いない', cn : '' },
+            { kana: 'そういない', mana: '相違ない', cn : '' },
+            { kana: '〜ぶ', mana: '〜部', cn : '' }
           ]
         }, {
           word: [
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' }
+            { kana: 'のびる', mana: '伸びる', type: '', cn: '' },
+            { kana: 'けっしん', mana: '決心', type: '', cn: '' },
+            { kana: 'ピアニスト', mana: '', type: '', cn: '' },
+            { kana: 'スタイル', mana: '', type: '', cn: '' },
+            { kana: 'やけい', mana: '夜景', type: '', cn: '' },
+            { kana: 'ぼうりょく', mana: '暴力', type: '', cn: '' }
           ],
           phrase: [
-            { kana: '', mana: '', cn : '' },
-            { kana: '', mana: '', cn : '' },
-            { kana: '', mana: '', cn : '' }
+            { kana: 'しん〜', mana: '新〜', cn : '' }
           ]
         }, {
           word: [
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' },
-            { kana: '', mana: '', type: '', cn: '' }
+            { kana: 'ごい', mana: '語彙', type: '', cn: '' },
+            { kana: 'たんご', mana: '単語', type: '', cn: '' },
+            { kana: 'もと', mana: '元', type: '', cn: '' },
+            { kana: 'ぶんるいする', mana: '分類する', type: '', cn: '' },
+            { kana: 'わご', mana: '和語', type: '', cn: '' },
+            { kana: 'かんご', mana: '漢語', type: '', cn: '' },
+            { kana: 'がいらいご', mana: '外来語', type: '', cn: '' },
+            { kana: 'こんしゅご', mana: '混種語', type: '', cn: '' },
+            { kana: 'わける', mana: '分ける', type: '', cn: '' },
+            { kana: 'もともと', mana: '', type: '', cn: '' },
+            { kana: 'とりいれる', mana: '取り入れる', type: '', cn: '' },
+            { kana: 'おんよみ', mana: '音読み', type: '', cn: '' },
+            { kana: 'もとづく', mana: '基づく', type: '', cn: '' },
+            { kana: 'せいよう', mana: '西洋', type: '', cn: '' },
+            { kana: 'ちゅうしん', mana: '中心', type: '', cn: '' },
+            { kana: 'かたかな', mana: '片仮名', type: '', cn: '' },
+            { kana: 'うち', mana: '', type: '', cn: '' },
+            { kana: 'くみあわせる', mana: '組み合わせる', type: '', cn: '' },
+            { kana: 'せいぶつ', mana: '生物', type: '', cn: '' },
+            { kana: 'どうぶつ', mana: '動物', type: '', cn: '' },
+            { kana: 'しょくぶつ', mana: '植物', type: '', cn: '' },
+            { kana: 'そうしょう', mana: '総称', type: '', cn: '' },
+            { kana: 'なまもの', mana: '生物', type: '', cn: '' },
+            { kana: 'にる', mana: '煮る', type: '', cn: '' },
+            { kana: 'ぶん', mana: '文', type: '', cn: '' },
+            { kana: 'ちがい', mana: '違い', type: '', cn: '' },
+            { kana: 'しゅくはくしせつ', mana: '宿泊施設', type: '', cn: '' },
+            { kana: 'たいてい', mana: '', type: '', cn: '' },
+            { kana: 'ふとん', mana: '布団', type: '', cn: '' },
+            { kana: 'しく', mana: '敷く', type: '', cn: '' }
           ],
           phrase: [
-            { kana: '', mana: '', cn : '' },
-            { kana: '', mana: '', cn : '' },
-            { kana: '', mana: '', cn : '' }
+            { kana: '〜せいき', mana: '〜世紀', cn : '' }
+          ]
+        }, {
+          word: [
+            { kana: 'はんだんする', mana: '判断する', type: '', cn: '' },
+            { kana: 'がんらい', mana: '元来', type: '', cn: '' },
+            { kana: 'ほんらい', mana: '本来', type: '', cn: '' },
+            { kana: 'バイオリン', mana: '', type: '', cn: '' },
+            { kana: 'おだやか', mana: '穏やか', type: '', cn: '' },
+            { kana: 'こうば', mana: '工場', type: '', cn: '' },
+            { kana: 'しきし', mana: '色紙', type: '', cn: '' },
+            { kana: 'いろがみ', mana: '色紙', type: '', cn: '' },
+            { kana: 'じつわ', mana: '実話', type: '', cn: '' },
+            { kana: 'こくりつ', mana: '国立', type: '', cn: '' },
+            { kana: 'こくりつだいがく', mana: '国立大学', type: '', cn: '' },
+            { kana: 'さんかしゃ', mana: '参加者', type: '', cn: '' },
+            { kana: 'しょうひん', mana: '賞品', type: '', cn: '' },
+            { kana: 'だいぶぶん', mana: '大部分', type: '', cn: '' },
+            { kana: 'かんしん', mana: '関心', type: '', cn: '' },
+            { kana: 'はっせいする', mana: '発生する', type: '', cn: '' },
+            { kana: 'やちん', mana: '家賃', type: '', cn: '' },
+            { kana: 'イカ', mana: '', type: '', cn: '' },
+            { kana: 'コレステロール', mana: '', type: '', cn: '' },
+            { kana: 'しょくひん', mana: '食品', type: '', cn: '' },
+            { kana: 'こうしゅう', mana: '広州', type: '', cn: '' },
+            { kana: 'じゅんばん', mana: '順番', type: '', cn: '' },
+            { kana: 'ゲームソフト', mana: '', type: '', cn: '' },
+            { kana: 'あくび', mana: '', type: '', cn: '' }
+          ],
+          phrase: []
+        }, {
+          word: [
+            { kana: 'ひとで', mana: '人手', type: '', cn: '' },
+            { kana: 'さいばん', mana: '裁判', type: '', cn: '' },
+            { kana: 'じっさい', mana: '実際', type: '', cn: '' },
+            { kana: 'かんどうする', mana: '感動する', type: '', cn: '' },
+            { kana: 'かつやくする', mana: '活躍する', type: '', cn: '' },
+            { kana: 'せんもんか', mana: '専門家', type: '', cn: '' },
+            { kana: 'げんご', mana: '言語', type: '', cn: '' },
+            { kana: 'せんこうする', mana: '専攻する', type: '', cn: '' },
+            { kana: 'こうばん', mana: '交番', type: '', cn: '' },
+            { kana: 'きんむする', mana: '勤務する', type: '', cn: '' },
+            { kana: 'おまわりさん', mana: 'お巡りさん', type: '', cn: '' },
+            { kana: 'テーブルクロス', mana: '', type: '', cn: '' },
+            { kana: 'ふろしき', mana: '風呂敷', type: '', cn: '' },
+            { kana: 'かべかざり', mana: '壁飾り', type: '', cn: '' },
+            { kana: 'てさげ', mana: '手提げ', type: '', cn: '' },
+            { kana: 'ふくろ', mana: '袋', type: '', cn: '' }
+          ],
+          phrase: [
+            { kana: '〜ぶそく', mana: '〜不足', cn : '' },
+            { kana: '〜がく', mana: '〜学', cn : '' }
           ]
         }
       ]
@@ -96,6 +176,118 @@ export default {
       grammar: [],
       words: [
         {
+          word: [
+            { kana: 'りゅうこしゅぞう', mana: '竜虎酒造', type: '', cn: '' },
+            { kana: 'なんと', mana: '', type: '', cn: '' },
+            { kana: 'キャプテン', mana: '', type: '', cn: '' },
+            { kana: 'ふうん', mana: '', type: '', cn: '' },
+            { kana: 'しんらいする', mana: '信頼する', type: '', cn: '' },
+            { kana: 'おひる', mana: 'お昼', type: '', cn: '' },
+            { kana: 'ながびく', mana: '長引く', type: '', cn: '' },
+            { kana: 'ねっしん', mana: '熱心', type: '', cn: '' },
+            { kana: 'めんどう', mana: '面倒', type: '', cn: '' },
+            { kana: 'まさに', mana: '', type: '', cn: '' },
+            { kana: 'りそう', mana: '理想', type: '', cn: '' }
+          ],
+          phrase: [
+            { kana: 'めんどうをみる', mana: '面倒を見る', cn : '' },
+            { kana: 'それはもう', mana: '', cn : '' },
+            { kana: 'きになる', mana: '気になる', cn : '' }
+          ]
+        }, {
+          word: [
+            { kana: 'かぶか', mana: '株価', type: '', cn: '' },
+            { kana: 'たった', mana: '', type: '', cn: '' },
+            { kana: 'しばい', mana: '芝居', type: '', cn: '' },
+            { kana: 'りょうかい', mana: '了解', type: '', cn: '' },
+            { kana: 'はいはい', mana: '', type: '', cn: '' },
+            { kana: 'せき', mana: '', type: '', cn: '' },
+            { kana: 'はやる', mana: '', type: '', cn: '' },
+            { kana: 'おしゃれする', mana: '', type: '', cn: '' },
+            { kana: 'めんどうくさい', mana: '面倒くさい', type: '', cn: '' },
+            { kana: 'ハンドバッグ', mana: '', type: '', cn: '' },
+            { kana: 'おっと', mana: '', type: '', cn: '' },
+            { kana: 'いきのこる', mana: '生き残る', type: '', cn: '' },
+            { kana: 'きせき', mana: '奇跡', type: '', cn: '' },
+            { kana: 'まつり', mana: '祭り', type: '', cn: '' },
+            { kana: 'たおれる', mana: '倒れる', type: '', cn: '' }
+          ],
+          phrase: []
+        }, {
+          word: [
+            { kana: 'あきはばら', mana: '秋葉原', type: '', cn: '' },
+            { kana: 'クイズばんぐみ', mana: 'クイズ番組', type: '', cn: '' },
+            { kana: 'さつえい', mana: '撮影', type: '', cn: '' },
+            { kana: 'しはらい', mana: '支払い', type: '', cn: '' },
+            { kana: 'リストラ', mana: '', type: '', cn: '' },
+            { kana: 'かいこする', mana: '解雇する', type: '', cn: '' },
+            { kana: 'さんせい', mana: '賛成', type: '', cn: '' },
+            { kana: 'アイデア', mana: '', type: '', cn: '' },
+            { kana: 'ぐうぜん', mana: '偶然', type: '', cn: '' },
+            { kana: 'ぎゃく', mana: '逆', type: '', cn: '' },
+            { kana: 'いとこ', mana: '', type: '', cn: '' },
+            { kana: 'いりょう', mana: '医療', type: '', cn: '' },
+            { kana: 'ぎろん', mana: '議論', type: '', cn: '' }
+          ],
+          phrase: []
+        }, {
+          word: [
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' }
+          ],
+          phrase: [
+            { kana: '', mana: '', cn : '' },
+            { kana: '', mana: '', cn : '' },
+            { kana: '', mana: '', cn : '' }
+          ]
+        }, {
+          word: [
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' }
+          ],
+          phrase: [
+            { kana: '', mana: '', cn : '' },
+            { kana: '', mana: '', cn : '' },
+            { kana: '', mana: '', cn : '' }
+          ]
+        }, {
+          word: [
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' },
+            { kana: '', mana: '', type: '', cn: '' }
+          ],
+          phrase: [
+            { kana: '', mana: '', cn : '' },
+            { kana: '', mana: '', cn : '' },
+            { kana: '', mana: '', cn : '' }
+          ]
+        }, {
           word: [
             { kana: '', mana: '', type: '', cn: '' },
             { kana: '', mana: '', type: '', cn: '' },

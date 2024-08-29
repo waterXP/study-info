a72c240512aca4ef6003daef0a61852b2d1ccb9f
waterXP <wutianba@sina.com>
2024-08-29 16:32:14 +0800

    up: update files

--- src/pages/JPTable/JPTable.js
+++ src/pages/JPTable/JPTable.js
@@ -3,11 +3,11 @@ import './JPTable.styl'
 import { useSearchParams } from 'react-router-dom'
 import Breadcrumb from '@com/Breadcrumb'
 import tables from '@/consts/jp/tables'
-import Box from '@com/Box'
 
 const JPTable = () => {
   const [searchParams] = useSearchParams()
   const [name, setName] = useState('')
+  const [desc, setDesc] = useState('')
   const [columns, setColumns] = useState([])
   const [dataSource, setDataSource] = useState([])
   useEffect(() => {
@@ -16,10 +16,11 @@ const JPTable = () => {
     if (pId && id) {
       const unit = tables.find(({ id }) => id === pId)
       if (unit) {
-        const target = unit.list.find(({ id }) => id === id)
+        const target = unit.list.find(({ id: uId }) => uId === id)
         if (target) {
-          const { name, columns, dataSource } = target
+          const { name, desc, columns, dataSource } = target
           setName(name)
+          setDesc(desc)
           setColumns(columns)
           setDataSource(dataSource)
         }
@@ -36,23 +37,32 @@ const JPTable = () => {
     [columns]
   )
   return (
-    <Box>
+    <div className='pg-jp-table'>
       <Breadcrumb to='/jp-tables' noTop>返回</Breadcrumb>
-      <p className='pg-jp-table_title'>{name}</p>
-      <div className='pg-jp-table_column is-header'>
-        {columns.map(v => (
-          <div key={v} className='pg-jp-table_cell is-header' style={style}>{v}</div>
-        ))}
-      </div>
-      {dataSource.map((v, i) => (
-        <div key={i} className='pg-jp-table_column'>
-          {v.map(v => (
-            <div key={v} className='pg-jp-table_cell' style={style}>{v}</div>
+      <div className='pg-jp-table_content hide-scroll'>
+        <p className='pg-jp-table_title'>{name}</p>
+        { desc && <p className='pg-jp-table_desc'>{desc}</p> }
+        <div className='pg-jp-table_column is-header'>
+          {columns.map(v => (
+            <div key={v} className='pg-jp-table_cell is-header' style={style}>{v}</div>
           ))}
         </div>
-      ))}
-      <div className='pg-jp-table_bottom' />
-    </Box>
+        {dataSource.map((v, i) => (
+          <div key={i} className='pg-jp-table_column'>
+            {v.map(v => (
+              <div key={v} className='pg-jp-table_cell' style={style}>
+                {
+                  Array.isArray(v)
+                    ? v.map((v, i) => <p key={i}>{v}</p>)
+                    : v
+                }
+              </div>
+            ))}
+          </div>
+        ))}
+        <div className='pg-jp-table_bottom' />
+      </div>
+    </div>
   )
 }
 
--- src/pages/JPTables/JPTables.js
+++ src/pages/JPTables/JPTables.js
@@ -2,6 +2,7 @@ import React, { memo, useCallback } from 'react'
 import './JPTables.styl'
 import { useNavigate } from 'react-router-dom'
 import tables from '@/consts/jp/tables'
+import Breadcrumb from '@com/Breadcrumb'
 import Box from '@com/Box'
 
 const JPTables = () => {
@@ -10,6 +11,7 @@ const JPTables = () => {
     navigate(`/jp-table?id=${id}&pId=${pId}`)
   }, [navigator])
   return <Box>
+    <Breadcrumb to='/' noTop>返回</Breadcrumb>
     {
       tables.map(
         ({ id: pId, title, desc, list }) => <div key={pId}>

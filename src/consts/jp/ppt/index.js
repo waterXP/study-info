1df9b5c8b12c0ef07d30a2754435ff145458e283
waterXP <wutianba@sina.com>
2024-09-23 09:59:21 +0800

    up: update files

--- src/consts/jp/ppt/index.js
+++ src/consts/jp/ppt/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import p01 from './01'
 
-const getTitle = title => title.map(
-  v => v.replace(
-    /<<.*?>>/g, str => <span>{str}</span>
-  )
+const getTitle = titles => titles.map(
+  title => {
+    const r = []
+    console.log('title', title)
+    title.split('<<').forEach(
+      v => {
+        if (v.includes('>>')) {
+          const [str, text] = v.split('>>')
+          r.push(<span>{str}</span>)
+          r.push(text)
+        } else {
+          r.push(v)
+        }
+      }
+    )
+    return <>
+      {
+        r.map((v, i) => <Fragment id={i}>{v}</Fragment>)
+      }
+    </>
+  }
 )
+
 
 const build = arr => arr.map(
   ({ title, explain, example, ...rest }) => ({

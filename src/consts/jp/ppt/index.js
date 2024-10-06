7d0b45326fb30c7a7044767323e44501fc08058c
陈文杰 <wutianba@sina.com>
2024-10-06 22:53:13 +0800

    up: update ppt

--- src/consts/jp/ppt/index.js
+++ src/consts/jp/ppt/index.js
@@ -2,6 +2,36 @@ import React, { Fragment } from 'react'
 import p01 from './01'
 import p02 from './02'
 import p03 from './03'
+import p04 from './04'
+import p05 from './05'
+import p06 from './06'
+import p07 from './07'
+import p08 from './08'
+import p09 from './09'
+import p10 from './10'
+import p11 from './11'
+import p12 from './12'
+import p13 from './13'
+import p14 from './14'
+import p15 from './15'
+import p16 from './16'
+
+const chDelete = str => {
+  if (typeof str === 'string' && str.includes('[[') && str.includes(']]')) {
+    const r = []
+    str.split('[[').forEach(v => {
+      if (v.includes(']]')) {
+        const [str, text] = v.split(']]')
+        r.push(<span className='glb-delete'>{str}</span>)
+        r.push(text)
+      } else {
+        r.push(v)
+      }
+    })
+    return r
+  }
+  return str
+}
 
 const trans = arr =>
   arr.map(line => {
@@ -27,14 +57,14 @@ const trans = arr =>
             } else {
               r.push(<span className='glb-cd'>{inStr}</span>)
             }
-            r.push(<span className='glb-cd'>{inText}</span>)
+            r.push(<span className='glb-cd'>{chDelete(inText)}</span>)
           })
         } else {
-          r.push(<span className='glb-cd'>{str}</span>)
+          r.push(<span className='glb-cd'>{chDelete(str)}</span>)
         }
-        r.push(text)
+        r.push(chDelete(text))
       } else {
-        r.push(v)
+        r.push(chDelete(v))
       }
     })
     return (
@@ -56,11 +86,11 @@ const trans = arr =>
                     </ruby>
                   )
                 } else {
-                  inlineR.push(str)
+                  inlineR.push(chDelete(str))
                 }
-                inlineR.push(text)
+                inlineR.push(chDelete(text))
               } else {
-                inlineR.push(v)
+                inlineR.push(chDelete(v))
               }
             })
             return (
@@ -84,4 +114,21 @@ const build = arr =>
     ...rest
   }))
 
-export default [p01, p02, p03].map(v => build(v))
+export default [
+  p01,
+  p02,
+  p03,
+  p04,
+  p05,
+  p06,
+  p07,
+  p08,
+  p09,
+  p10,
+  p11,
+  p12,
+  p13,
+  p14,
+  p15,
+  p16
+].map(v => build(v))

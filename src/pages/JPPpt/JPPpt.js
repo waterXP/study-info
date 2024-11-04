9170de7eb24d8b67931f9dfb432b64ec207476c8
waterXP <wutianba@sina.com>
2024-11-04 10:31:08 +0800

    up: udpate files

--- src/pages/JPPpt/JPPpt.js
+++ src/pages/JPPpt/JPPpt.js
@@ -1,12 +1,14 @@
-import React, { memo, useEffect, useState } from 'react'
+import React, { memo, useRef, useEffect, useState, useCallback } from 'react'
 import './JPPpt.styl'
-import { useSearchParams } from 'react-router-dom'
+import { useSearchParams, useNavigate } from 'react-router-dom'
 import ppt from '@/consts/jp/ppt'
 import Breadcrumb from '@com/Breadcrumb'
 
 const JPPpt = () => {
+  const navigate = useNavigate()
   const [searchParams] = useSearchParams()
   const [unit, setUnit] = useState(null)
+  const refId = useRef(null)
   useEffect(() => {
     const id = searchParams.get('id')
     if (id) {
@@ -16,10 +18,57 @@ const JPPpt = () => {
         return unit
       })
       if (unit) {
+        refId.current = id
         setUnit(unit)
       }
     }
   }, [searchParams])
+  const onPrevClick = useCallback(() => {
+    if (refId.current) {
+      let mainIndex = 0
+      let subIndex = 0
+      ppt.some((v, i) => {
+        mainIndex = i
+        const index = v.findIndex(v => v.id === refId.current)
+        if (~index) {
+          subIndex = index
+        }
+        return ~index
+      })
+      if (subIndex > 0) {
+        const tar = ppt[mainIndex][subIndex - 1]
+        navigate(`/jp-ppt?&id=${tar.id}`, { replace: true })
+      } else {
+        mainIndex = mainIndex > 0 ? mainIndex - 1 : ppt.length - 1
+        subIndex = ppt[mainIndex].length - 1
+        const tar = ppt[mainIndex][subIndex]
+        navigate(`/jp-ppt?&id=${tar.id}`, { replace: true })
+      }
+    }
+  }, [])
+  const onNextClick = useCallback(() => {
+    if (refId.current) {
+      let mainIndex = 0
+      let subIndex = 0
+      ppt.some((v, i) => {
+        mainIndex = i
+        const index = v.findIndex(v => v.id === refId.current)
+        if (~index) {
+          subIndex = index
+        }
+        return ~index
+      })
+      if (subIndex < ppt[mainIndex].length - 1) {
+        const tar = ppt[mainIndex][subIndex + 1]
+        navigate(`/jp-ppt?&id=${tar.id}`, { replace: true })
+      } else {
+        mainIndex = mainIndex < ppt.length - 1 ? mainIndex + 1 : 0
+        subIndex = 0
+        const tar = ppt[mainIndex][subIndex]
+        navigate(`/jp-ppt?&id=${tar.id}`, { replace: true })
+      }
+    }
+  }, [])
   return (
     <div className='pg-jp-ppt'>
       <Breadcrumb to='/jp-ppts' noTop>
@@ -54,6 +103,22 @@ const JPPpt = () => {
           </div>
         </div>
       )}
+      <div className='pg-jp-ppt_footer'>
+        <div className='pg-jp-ppt_footer-buttons'>
+          <div
+            className='pg-jp-ppt_footer-button on-click'
+            onClick={onPrevClick}
+          >
+            上一个
+          </div>
+          <div
+            className='pg-jp-ppt_footer-button is-right on-click'
+            onClick={onNextClick}
+          >
+            下一个
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

2a0f6660d9d94da26472b3f9277f9656c5869c70
陈文杰 <wutianba@sina.com>
2024-11-28 02:30:01 +0800

    up: udpate files

--- src/consts/jp/exec/listen.js
+++ src/consts/jp/exec/listen.js
@@ -1,4 +1,7 @@
 import listen1 from './listen1'
 import listen2 from './listen2'
+import listen3 from './listen3'
+import listen4 from './listen4'
+import listen5 from './listen5'
 
-export default [...listen1, ...listen2]
+export default [...listen1, ...listen2, ...listen3, ...listen4, ...listen5]

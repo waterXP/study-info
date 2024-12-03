5d7bc60f823d5af82ba674ca280a29209b81f9da
waterXP <wutianba@sina.com>
2024-12-03 15:42:15 +0800

    up: update files

---
+++ src/consts/jp/grammar/grammar4.js
@@ -0,0 +1,23 @@
+export default {
+  select: [
+    {
+      id: '',
+      q: [''],
+      a: [['']]
+    }
+  ],
+  combination: [
+    {
+      id: '',
+      q: [''],
+      a: [[]]
+    }
+  ],
+  essay: [
+    {
+      id: '',
+      q: [''],
+      a: [[]]
+    }
+  ]
+}

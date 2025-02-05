6c0c9cd736480cf0157877ebe55f11a6020cda63
waterXP <wutianba@sina.com>
2025-02-05 11:29:26 +0800

    feat: update create order

--- src/index.js
+++ src/index.js
@@ -6,10 +6,27 @@ import './index.styl'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+const plusReady = () => {
+  // eslint-disable-next-line no-alert
+  window.alert(
+    window.plus.android.invoke(
+      'com.dcp.system.facade.SystemFacade',
+      'getDeviceCode'
+    )
+  )
+}
+
+if (window.plus) {
+  plusReady()
+} else {
+  document.addEventListener('plusready', plusReady, false)
+}
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Provider store={store}><App /></Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
 
--- src/pages/Cabinet/Cabinet.js
+++ src/pages/Cabinet/Cabinet.js
@@ -2,6 +2,7 @@ import React, { memo, useCallback } from 'react'
 import './Cabinet.styl'
 import { useNavigate } from 'react-router-dom'
 import CabinetBody from '@/components/CabinetBody'
+import Icon from '@/components/Icon'
 
 const Cabinet = () => {
   const navigate = useNavigate()
@@ -17,16 +18,39 @@ const Cabinet = () => {
   return (
     <CabinetBody>
       <div className='pg-cabinet'>
-        <div className='pg-cabinet_save on-click' onClick={gotoSave}>
-          <span className='pg-cabinet_text'>存件</span>
+        <div className='pg-cabinet_header'>
+          <div className='pg-cabinet_header-left'>
+            <Icon className='pg-cabinet_header-icon' type='icon-mall-l' />
+            <span className='pg-cabinet_header-title'>智慧快递柜</span>
+          </div>
+          <span className='pg-cabinet_header-right'>登录/注册</span>
         </div>
-        <div className='pg-cabinet_pick'>
-          <div className='pg-cabinet_pick-self on-click' onClick={gotoPick}>
-            <span className='pg-cabinet_text'>取件</span>
+        <div className='pg-cabinet_body'>
+          <div className='pg-cabinet_save on-click' onClick={gotoSave}>
+            <span className='pg-cabinet_text'>存件</span>
+            <span className='pg-cabinet_tip'>快速存放您的包裹</span>
           </div>
-          <div className='pg-cabinet_pick-others on-click' onClick={gotoCode}>
-            <span className='pg-cabinet_text'>代取件</span>
+          <div className='pg-cabinet_pick'>
+            <div className='pg-cabinet_pick-self on-click' onClick={gotoPick}>
+              <span className='pg-cabinet_text'>取件</span>
+              <span className='pg-cabinet_tip'>便捷取出您的包裹</span>
+            </div>
+            <div className='pg-cabinet_pick-others on-click' onClick={gotoCode}>
+              <span className='pg-cabinet_text'>代取件</span>
+              <span className='pg-cabinet_tip'>帮他人代取包裹</span>
+            </div>
           </div>
+        </div>
+        <div className='pg-cabinet_footer'>
+          <div className='pg-cabinet_footer-version'>
+            <span className='pg-cabinet_footer-text'>系统版本: v1.0.0</span>
+            <span className='pg-cabinet_footer-text'>
+              客服电话: 400-888-8888
+            </span>
+          </div>
+          <span className='pg-cabinet_footer-right'>
+            © 2024 智慧快递柜 版权所有
+          </span>
         </div>
       </div>
     </CabinetBody>

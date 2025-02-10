33c4e38682ac615b28ef73dfb8a4d2e061e3ec15
waterXP <wutianba@sina.com>
2025-02-10 19:12:09 +0800

    feat: update create order

--- src/api/expressLocker.js
+++ src/api/expressLocker.js
@@ -2,4 +2,12 @@ import api from './index'
 
 export const findUser = p => api.get('/expressLocker/findUser', p)
 
+export const getAvailableLockerBox = p => api.get('/expressLocker/getAvailableLockerBox', p)
+
+export const saveExpress = p => api.post('/expressLocker/saveExpress', p)
+
+export const findWaitTakeList = p => api.post('/expressLocker/findWaitTakeList', p)
+
+export const takeExpress = p => api.post('/expressLocker/takeExpress', p)
+
 export default null

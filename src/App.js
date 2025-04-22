374319bd4ebbda8169b72fceb3c574f25b76a9d7
陈文杰 <wutianba@sina.com>
2025-04-22 21:34:41 +0800

    up: update structure

--- src/App.js
+++ src/App.js
@@ -27,6 +27,10 @@ import {
 import { updateInstance } from '@/api'
 import { saveExpress, takeExpress } from '@/api/expressLocker'
 
+message.config({
+  top: 100, duration: 500000
+})
+
 const ComMap = {
   default: Cabinet,
   pick: CabinetPick,
